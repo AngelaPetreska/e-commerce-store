e42bf0486c3eb16398d81f76879ae5d6768d7794
Angela Petreska <angelapetreska997@gmail.com>
2024-11-04 18:30:48 +0100

    BannerGrid.jsx was updated

--- e-commerce-store-project/src/components/BannerGrid.jsx
+++ e-commerce-store-project/src/components/BannerGrid.jsx
@@ -5,20 +5,20 @@ import BannerGrid3 from '../assets/BannerGrid3.jpg';
 
 function BannerGrid() {
   return (
-    <div className="m-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      <div className="col-start-1 col-end-4 row-start-1 row-end-5">
+    <div className="m-10 grid grid-cols-2 gap-4">
+      <div className="col-span-1 row-span-2">
         <Link to="/products/category/jewelery">
-          <img src={BannerGrid1} alt="Banner Grid 1" />
+          <img src={BannerGrid1} alt="Banner Grid 1" className="w-full h-full object-cover" />
         </Link>
       </div>
-      <div className="col-start-4 col-end-7 row-start-1 row-end-3">
+      <div className="col-span-1 row-span-1">
         <Link to="/products/category/men's%20clothing">
-          <img src={BannerGrid2} alt="Banner Grid 2" />
+          <img src={BannerGrid2} alt="Banner Grid 2" className="w-full h-full object-cover" />
         </Link>
       </div>
-      <div className="col-start-4 col-end-7 row-start-3 row-end-5">
+      <div className="col-span-1 row-span-1">
         <Link to="/products/category/women's%20clothing">
-          <img src={BannerGrid3} alt="Banner Grid 3" />
+          <img src={BannerGrid3} alt="Banner Grid 3" className="pb-3 w-full h-full object-cover" />
         </Link>
       </div>
     </div>
