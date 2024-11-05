fd7b4c5ac86beefc683538480af3ffa56f3ae4c5
Angela Petreska <angelapetreska997@gmail.com>
2024-11-05 20:55:35 +0100

    CheckoutPage.jsx was updated

--- e-commerce-store-project/src/components/CheckoutPage.jsx
+++ e-commerce-store-project/src/components/CheckoutPage.jsx
@@ -1,36 +1,40 @@
 import { useLocation } from 'react-router-dom';
+import NavigationBar from './NavigationBar';
+import Footer from './Footer';
 
 function CheckoutPage() {
+
   const { state } = useLocation();
+
   const { product, size, quantity } = state || {};
-
+  
   if (!product) {
-    return <div>Error: No product data provided</div>;
-  }
+        return <div>Error: No product data provided</div>;
+      }
 
-  const orderSummary = (
-    <div className="order-summary">
-      <h2>Order Summary</h2>
-      <div className="product-item">
-        <p>{product.title}</p>
-        <p>Size: {size}</p>
-        <p>Quantity: {quantity}</p>
-        <p>Price: ${product.price * quantity}</p>
+ const orderSummary = (
+  <div className="order-summary">
+    <h2>Order Summary</h2>
+    <div className="product-item">
+      <p>{product.title}</p>
+      <p>Size: {size}</p>
+      <p>Quantity: {quantity}</p>
+      <p>Price: ${product.price * quantity}</p>
       </div>
       <p className="shipping">Shipping: $0.00</p>
       <p className="tax">Tax: $0.00</p>
       <p className="total-price">Total Price: ${product.price * quantity}</p>
-    </div>
-  );
-
-  return (
-    <div className="checkout-page">
-      <NavigationBar />
-      {orderSummary}
-      {/* Add CARD ICON section */}
-      <Footer />
-    </div>
-  );
+      </div>
+      );
+      return (
+      <div className="checkout-page">
+        <NavigationBar />
+        {orderSummary}
+        {/* Add CARD ICON section */}
+        <Footer />
+        </div>
+        );
 }
 
 export default CheckoutPage;
+
