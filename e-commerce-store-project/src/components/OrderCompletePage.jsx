195949baa85e2fcf1ab09507f16e4c521afc03b4
Angela Petreska <angelapetreska997@gmail.com>
2024-11-09 19:41:36 +0100

    OrderCompletePage.jsx was updated

--- e-commerce-store-project/src/components/OrderCompletePage.jsx
+++ e-commerce-store-project/src/components/OrderCompletePage.jsx
@@ -47,23 +47,34 @@ function OrderCompletePage() {
   return (
     <div>
       <NavigationBar />
-      <p className='font-poppins text-4xl font-medium leading-tight tracking-tighter text-center'> Complete! </p>
+      <p className="font-poppins text-4xl font-medium leading-tight tracking-tighter text-center"> Complete! </p>
       <NavigationCheckoutBar
         activeStep={activeStep}
         steps={steps}
         stepThreeIcon={stepThreeDone}
       />
-      <div className="container mx-auto py-16 px-4">
-        <h1 className="text-3xl font-bold leading-tight tracking-tighter text-center">Order Complete</h1>
-        <div className="mt-8 border rounded-lg p-4">
-          <p className="text-lg font-medium">Order Details</p>
-          <ul className="list-disc mt-4 ml-4">
-            <li>Order Code: {orderCode}</li>
-            <li>Date: {orderDate}</li>
-            <li>Total: ${total.toFixed(2)}</li>
-            <li>Payment Method: {isLoadingPaymentMethod ? 'Loading...' : selectedPaymentMethod?.name ? selectedPaymentMethod.name : 'Credit Card / PayPal'}</li>
-          </ul>
-        </div>
+      
+      <div className='flex justify-center'>
+      <div className="py-16 w-3/5 mx-14 my-14 shadow-[0px_20px_20px_10px_#00000024]">
+        <h1 className="text-3xl font-bold leading-tight tracking-tighter text-center text-custom-grey"> Thank you! 🎉 </h1>
+        <div className="mt-8">
+          <p className="font-poppins text-3xl font-medium leading-tight tracking-normal text-center text-[#22637E]">Your order has been received</p>
+          <div className="mt-8 bg-white  flex flex-row justify-center gap-4">
+  <ul className="flex flex-col items-center gap-2 list-none font-inter text-sm font-semibold leading-7 text-left text-custom-grey">
+    <li>Order</li>
+    <li>Date:</li>
+    <li>Total:</li>
+    <li>Payment Method:</li>
+  </ul>
+  <ul className="flex flex-col items-center gap-2 list-none text-custom-black font-inter text-sm font-semibold leading-7 text-left ">
+    <li>{orderCode}</li>
+    <li>{orderDate}</li>
+    <li>${total.toFixed(2)}</li>
+    <li>{isLoadingPaymentMethod ? 'Loading...' : selectedPaymentMethod?.name ? selectedPaymentMethod.name : 'Credit Card / PayPal'}</li>
+  </ul>
+</div>
+  </div>
+      </div>
       </div>
 
       <Footer />
