2d35de6f015d832448e8bcbd2184fe536944876a
Angela Petreska <angelapetreska997@gmail.com>
2024-11-10 16:26:11 +0100

    SignIn.jsx and SignUp.jsx were updated

--- e-commerce-store-project/src/components/SignIn.jsx
+++ e-commerce-store-project/src/components/SignIn.jsx
@@ -19,25 +19,48 @@ const SignIn = () => {
       <div className="flex-1 relative">
         <img src={SignInSignUp} alt="Sign In/Sign Up" className="absolute top-0 left-0 w-full h-auto object-cover" />
       </div>
-      <div className="flex-1 bg-white items-center">
-        <div className='mx-10 my-10 px-8 py-24'>
-          <h2 className="mb-4 font-poppins text-4xl font-medium leading-10 tracking-tighter text-left">Sign In</h2>
+      <div className="flex-1 bg-white items-center px-8 py-12">
+        <div className="mx-auto w-full px-4 py-8">
+          <h2 className="text-3xl md:text-4xl font-medium text-center">Sign In</h2>
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
             <div>
               <label htmlFor="email" className="block text-sm"></label>
-              <input type="email" id="email" {...register('email')} placeholder="Enter your email" className="mt-1 block w-full px-3 py-2 bg-white border-b-2 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" autocomplete="username" />
+              <input
+                type="email"
+                id="email"
+                {...register('email')}
+                placeholder="Enter your email"
+                className="mt-1 block w-full px-3 py-2 bg-white border-b-2 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                autoComplete="username"
+              />
             </div>
             <div>
               <label htmlFor="password" className="block text-sm"></label>
-              <input type="password" id="password" {...register('password')} placeholder="Password" className="mt-1 block w-full px-3 py-2 bg-white border-b-2 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" autocomplete="current-password" />
+              <input
+                type="password"
+                id="password"
+                {...register('password')}
+                placeholder="Password"
+                className="mt-1 block w-full px-3 py-2 bg-white border-b-2 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                autoComplete="current-password"
+              />
             </div>
             <div className="flex items-center">
-              <input type="checkbox" id="rememberMe" {...register('rememberMe')} className="mr-2 border-gray-300 rounded-md shadow-sm" />
-              <label htmlFor="rememberMe" className="text-sm text-custom-grey">Remember me</label>
+              <input
+                type="checkbox"
+                id="rememberMe"
+                {...register('rememberMe')}
+                className="mr-2 border-gray-300 rounded-md shadow-sm"
+              />
+              <label htmlFor="rememberMe" className="text-sm text-gray-700">Remember me</label>
               <Link to="/forgot-password" className="text-custom-blue font-semibold ml-auto">Forgot password?</Link>
             </div>
-            <button type="submit" className="bg-custom-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">Sign In</button>
-            <p className="text-custom-grey mt-4 text-center">Don't have an account? <Link to="/" className="text-custom-blue font-semibold">Sign Up</Link></p>
+            <button type="submit" className="bg-custom-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">
+              Sign In
+            </button>
+            <p className="text-custom-grey mt-4 text-center">
+              Don't have an account? <Link to="/" className="text-custom-blue font-semibold">Sign Up</Link>
+            </p>
           </form>
         </div>
       </div>
--- e-commerce-store-project/src/components/SignUp.jsx
+++ e-commerce-store-project/src/components/SignUp.jsx
@@ -19,26 +19,64 @@ const SignUp = () => {
       <div className="flex-1 relative">
         <img src={SignInSignUp} alt="Sign In/Sign Up" className="absolute top-0 left-0 w-full h-auto object-cover" />
       </div>
-      <div className="flex-1 bg-white items-center">
-        <div className='mx-10 my-10 px-8 py-24'>
-          <h2 className="mb-4 font-poppins text-4xl font-medium leading-10 tracking-tighter text-left">Sign Up</h2>
-          <p className="text-custom-grey mb-6 font-inter text-base leading-7 text-left">Already have an account? <Link to="/sign-in" className="text-blue-500"><span className='text-custom-blue font-medium font-inter text-base leading-7 text-left'>Sign In</span></Link></p>
+      <div className="flex-1 bg-white items-center px-8 py-12">
+        <div className="mx-auto w-full px-4 py-8">
+          <h2 className="text-3xl md:text-4xl font-medium text-center mb-6">Sign Up</h2>
+          <p className="text-gray-500 text-center mb-6">Already have an account? <Link to="/sign-in" className="text-blue-500">Sign In</Link></p>
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
             <div>
-              <label htmlFor="username" className="block text-sm"></label>
-              <input type="text" id="username" {...register('username')} placeholder="Username" className="text-custom-grey mt-1 block w-full px-3 py-2 bg-white border-b-2 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" autocomplete="username" />
+              <label htmlFor="username" className="block text-sm font-medium text-gray-700">Username</label>   
+
+              <input
+                type="text"
+                id="username"
+                {...register('username')}   
+
+                placeholder="Enter your username"
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500   
+ focus:border-indigo-500"   
+
+                autoComplete="username"
+              />
             </div>
             <div>
-              <label htmlFor="email" className="block text-sm"></label>
-              <input type="email" id="email" {...register('email')} placeholder="Email" className="text-custom-grey mt-1 block w-full px-3 py-2 bg-white border-b-2 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" autocomplete="email" />
+              <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email address</label>
+              <input
+                type="email"
+                id="email"   
+
+                {...register('email')}
+                placeholder="Enter your email address"
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"   
+
+                autoComplete="email"
+              />
             </div>
             <div>
-              <label htmlFor="password" className="block text-sm"></label>
-              <input type="password" id="password" {...register('password')} placeholder="Password" className="text-custom-grey mt-1 block w-full px-3 py-2 bg-white border-b-2 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" autocomplete="new-password" />
+              <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+              <input
+                type="password"   
+
+                id="password"
+                {...register('password')}   
+
+                placeholder="Enter your password"
+                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500   
+ focus:border-indigo-500"   
+
+                autoComplete="new-password"
+              />
             </div>
-            <div>
-              <input type="checkbox" id="terms" {...register('terms', { required: true })} className="mr-2 border-gray-300 rounded-md shadow-sm" />
-              <label htmlFor="terms" className="text-custom-grey mb-6 font-inter text-base leading-7 text-left">I agree with <span className='text-custom-black font-medium'>Privacy Policy</span> and <span className='text-custom-black font-medium'>Terms of Use</span></label>
+            <div className="flex items-center">
+              <input
+                id="terms"
+                type="checkbox"
+                {...register('terms', { required: true })}
+                className="mr-2 border border-gray-300 rounded-md shadow-sm"
+              />
+              <label htmlFor="terms" className="text-sm text-gray-700">
+                I agree to the <a href="#" className="text-blue-500">Terms of Service</a> and <a href="#" className="text-blue-500">Privacy Policy</a>
+              </label>
               {errors.terms && <p className="text-red-500 text-sm">Please agree to the terms and conditions.</p>}
             </div>
             <button type="submit" className="bg-custom-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full">Sign Up</button>
