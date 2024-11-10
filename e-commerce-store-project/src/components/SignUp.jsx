3914370c3fff933f1c781299e3ed460845b5c2ef
Angela Petreska <angelapetreska997@gmail.com>
2024-11-10 15:12:45 +0100

    SignUp.jsx was updated

--- e-commerce-store-project/src/components/SignUp.jsx
+++ e-commerce-store-project/src/components/SignUp.jsx
@@ -1,7 +1,7 @@
 import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import useAuthStore from '../store/authStore';
-import { Link } from 'react-router-dom';
+import SignInSignUp from '../assets/SignInSignUp.svg';
 
 const SignUp = () => {
   const { register, handleSubmit } = useForm();
@@ -11,22 +11,40 @@ const SignUp = () => {
   const onSubmit = (data) => {
     console.log('User registered:', data);
     login(data);
-    navigate('/home-page'); 
+    navigate('/home-page');
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label htmlFor="username">Username:</label>
-        <input type="text" id="username" {...register('username')} />
+    <div className="flex h-screen">
+      <div className="flex-1 relative">
+        <img src={SignInSignUp} alt="Sign In/Sign Up" className="absolute top-0 left-0 w-full h-auto object-cover" />
       </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" {...register('password')} />
+      <div className="flex-1 bg-white items-center">
+        <div className='mx-10 my-10'>
+          <h2 className="mb-4 font-poppins text-4xl font-medium leading-10 tracking-tighter text-left">Sign Up</h2>
+          <p className="text-custom-grey mb-6 font-inter text-base leading-7 text-left">Already have an account? <Link to="/sign-in" className="text-blue-500"><span className='text-custom-blue font-medium font-inter text-base leading-7 text-left'>Sign In</span></Link></p>
+          <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+            <div>
+              <label htmlFor="username" className="block text-sm"></label>
+              <input type="text" id="username" {...register('username')} placeholder="Username" className="text-custom-grey font-inter text-base leading-7 text-left mt-1 block w-full px-3 py-2 bg-white border-b-2 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            </div>
+            <div>
+              <label htmlFor="email" className="block text-sm"></label>
+              <input type="email" id="email" {...register('email')} placeholder="Email" className="text-custom-grey font-inter text-base leading-7 text-left mt-1 block w-full px-3 py-2 bg-white border-b-2 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            </div>
+            <div>
+              <label htmlFor="password" className="block text-sm"></label>
+              <input type="password" id="password" {...register('password')} placeholder="Password" className="text-custom-grey font-inter text-base leading-7 text-left mt-1 block w-full px-3 py-2 bg-white border-b-2 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+            </div>
+            <div>
+              <input type="checkbox" id="terms" {...register('terms', { required: true })} className="mr-2 border-gray-300 rounded-md shadow-sm" />
+              <label htmlFor="terms" className="text-custom-grey mb-6 font-inter text-base leading-7 text-left">I agree with <span className='text-custom-black font-medium'>Privacy Policy</span> and <span className='text-custom-black font-medium'>Terms of Use</span></label>
+            </div>
+            <button type="submit" className="bg-custom-blue hover:bg-blue-700 text-white py-2 px-4 rounded-md font-medium font-inter text-normal leading-7 w-full">Sign Up</button>
+          </form>
+        </div>
       </div>
-      <button type="submit">Sign Up</button>
-      <p>Already have an account? <Link to="/sign-in">Sign In</Link></p>
-    </form>
+    </div>
   );
 };
 
