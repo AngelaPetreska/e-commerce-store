d5da1a173d076c4f10676124ac5ff072baf7746a
Angela Petreska <angelapetreska997@gmail.com>
2024-11-10 12:25:56 +0100

    NavigationBar.jsx was updated

--- e-commerce-store-project/src/components/NavigationBar.jsx
+++ e-commerce-store-project/src/components/NavigationBar.jsx
@@ -3,8 +3,9 @@ import React, { useState, useEffect } from 'react';
 import SearchIcon from '../components/icons/SearchIcon';
 import UserIcon from '../components/icons/UserIcon';
 import CartIcon from '../components/icons/CartIcon';
-import useCartStore from '../store/cartStore'; 
+import useCartStore from '../store/cartStore';
 import Badge from '../components/Badge';
+import SearchMenu from '../components/SearchMenu';
 
 async function getCategories() {
   const response = await fetch('https://fakestoreapi.com/products/categories');
@@ -14,48 +15,45 @@ async function getCategories() {
 
 function NavigationBar() {
   const [categories, setCategories] = useState([]);
-  const { cartItems } = useCartStore(); // Access cart items from the store
-
-
+  const { cartItems } = useCartStore();
+ 
   useEffect(() => {
     const fetchCategories = async () => {
       const data = await getCategories();
       setCategories(data);
     };
-     fetchCategories();
-     }, []);
+    fetchCategories();
+  }, []);
 
-     const HOME_LINK_KEY = 'home';
-     
-     return (
-     <nav className="flex items-center justify-between px-4 py-2 bg-white">
+  const HOME_LINK_KEY = 'home';
+
+  return (
+    <nav className="flex items-center justify-between px-4 py-2 bg-white">
       <div className="w-32 h-6 text-custom-blue bg-white font-poppins text-2xl font-semibold leading-6 text-center pl-3;">UrbanNest</div>
       <ul className="gap-x-10 text-transform: capitalize flex items-center justify-between text-custom-grey bg-white font-space-grotesk text-sm font-medium leading-6 text-left">
         <li key={HOME_LINK_KEY} className="hover:text-custom-blue">
           <Link to="/">Home</Link>
+        </li>
+        {categories.map((category) => (
+          <li key={category} className="hover:text-custom-blue">
+            <Link to={`/products/category/${category}`}>{category}</Link>
           </li>
-          {categories.map((category) => (
-            <li key={category} className="hover:text-custom-blue">
-              <Link to={`/products/category/${category}`}>{category}</Link>
-              </li>
-            ))}
-            <li key="contact-us" className="hover:text-custom-blue">
-              <Link to="/contact-us">Contact Us</Link>
-              </li>
-              </ul>
-              <div className="flex flex-row items-center gap-x-4">
-                <button><SearchIcon /></button>
-                <Link to="/account"><button className='mt-2'><UserIcon /></button></Link>
-                <Link to="/checkout"><button className='mt-2'><CartIcon />
-                {/* Conditionally render Badge component */}
-                {cartItems.length > 0 && (
-  <Badge count={cartItems.length} className="bg-custom-blue text-white px-1 rounded-full" />
-)} </button></Link>
-                  </div>
-                   </nav>
-                   );
-                  }
-
-
+        ))}
+        <li key="contact-us" className="hover:text-custom-blue">
+          <Link to="/contact-us">Contact Us</Link>
+        </li>
+      </ul>
+      <div className="flex flex-row items-center gap-x-4">
+        <button><SearchIcon /></button>
+        <Link to="/account"><button className='mt-2'><UserIcon /></button></Link>
+        <Link to="/checkout"><button className='mt-2'><CartIcon />
+          {cartItems.length > 0 && (
+            <Badge count={cartItems.length} className="bg-custom-blue text-white px-1 rounded-full" />
+          )}
+        </button></Link>
+      </div>
+    </nav>
+  );
+}
 
 export default NavigationBar;
